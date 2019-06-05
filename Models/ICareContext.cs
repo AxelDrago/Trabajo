2065e116551d46e4181113b1a2322079b6ca8e5e
Paul Solano <paulsolano2000@gmail.com>
2019-06-05 15:30:16 -0500

    REcuperacion2

--- Models/ICareContext.cs
+++ Models/ICareContext.cs
@@ -1,7 +1,13 @@
-namespace Trabajo.Models
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Trabajo.Models;
+namespace Proyecto_Multiplataforma.Models
 {
-    public class ICareContext
+    public class ICareContext: IdentityDbContext
     {
-        
+         public DbSet<Mascota> Mascotas { get; set; }
+
+        public ICareContext(DbContextOptions<ICareContext> options) : base(options) { }
+
     }
 }
