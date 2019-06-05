865464c7fa140b51a2f55a198a3ab9a24c38fbfd
Paul Solano <paulsolano2000@gmail.com>
2019-06-05 15:23:50 -0500

    Recuperacion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Trabajo.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace Trabajo.Controllers
+namespace Proyecto_Multiplataforma.Controllers
 {
     public class HomeController: Controller
     {
-        
+         public IActionResult RegistroMascota()
+        {
+            ViewBag.Tipos = _context.Tipos.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistroMascota(Mascota m)
+        {
+            if (ModelState.IsValid){
+                _context.Add(m);
+                _context.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+            ViewBag.Tipos = _context.Tipos.ToList();
+
+            return View();
+        }
     }
 }
