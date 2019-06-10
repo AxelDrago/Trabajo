4f940b5cc4e5858b35cd131bdb053d4629c1c9c1
SebastianAparicio <bruno.aparicio@usil.pe>
2019-06-10 18:05:06 -0500

    Descripcion2

---
+++ Migrations/20190610230244_Descripcion.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trabajo.Migrations
+{
+    public partial class Descripcion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Tipos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raza",
+                table: "Mascotas",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NombreTemporal",
+                table: "Mascotas",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Foto",
+                table: "Mascotas",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Mascotas",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Mascotas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Tipos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raza",
+                table: "Mascotas",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NombreTemporal",
+                table: "Mascotas",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Foto",
+                table: "Mascotas",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- Models/TipoMascota.cs
+++ Models/TipoMascota.cs
@@ -1,10 +1,13 @@
 
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Trabajo.Models
 {
     public class TipoMascota
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
         public List<Mascota> Mascotas { get; set; }
     }
