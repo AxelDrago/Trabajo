108629fba60feea7921ca714e1a9b2edfa8a77a7
SebastianAparicio <bruno.aparicio@usil.pe>
2019-06-12 16:18:48 -0500

    Sexo

---
+++ Migrations/20190612211532_Registro.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trabajo.Migrations
+{
+    public partial class Registro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sexo",
+                table: "Mascotas",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sexo",
+                table: "Mascotas");
+        }
+    }
+}
