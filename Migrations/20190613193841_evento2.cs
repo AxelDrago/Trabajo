8ad2f65993821ae85707df09f65ff3614f98410a
AxelDrago <axeljoeldragoespinoza@gmail.com>
2019-06-13 14:40:27 -0500

    terminandoevento

---
+++ Migrations/20190613193841_evento2.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trabajo.Migrations
+{
+    public partial class evento2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evento_TipoC_TipoEventoId",
+                table: "Evento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Evento_TipoEventoId",
+                table: "Evento");
+
+            migrationBuilder.DropColumn(
+                name: "TipoEventoId",
+                table: "Evento");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evento_TipoId",
+                table: "Evento",
+                column: "TipoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evento_TipoC_TipoId",
+                table: "Evento",
+                column: "TipoId",
+                principalTable: "TipoC",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evento_TipoC_TipoId",
+                table: "Evento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Evento_TipoId",
+                table: "Evento");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoEventoId",
+                table: "Evento",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evento_TipoEventoId",
+                table: "Evento",
+                column: "TipoEventoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evento_TipoC_TipoEventoId",
+                table: "Evento",
+                column: "TipoEventoId",
+                principalTable: "TipoC",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
