df76e8d5f1928608451b372225d7cf7412631abd
Sebastian Aparicio <sebastian.yisk@gmail.com>
2019-06-15 14:54:58 -0500

    Telefono

---
+++ Migrations/20190615195152_Telefonos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trabajo.Migrations
+{
+    public partial class Telefonos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Telefono",
+                table: "Mascotas",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefono",
+                table: "Mascotas");
+        }
+    }
+}
