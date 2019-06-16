faeeaa5a8ba057a55a026ed07f88a52ecc5f3dbd
noboruaso <noboru.aso@usil.pe>
2019-06-15 19:05:38 -0500

    filtros-mascotas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace Trabajo.Controllers
     public class HomeController: Controller
     {
 
-        public IActionResult Index(int tipomascota)
+        public IActionResult Index(int tipomascota, int sexomascota, int edadmascota)
         {
             ViewBag.Tipos = _context.Tipos.ToList();
             var mascotas = _context.Mascotas.Include(x => x.Tipo).ToList();
@@ -22,6 +22,22 @@ namespace Trabajo.Controllers
             if(tipomascota != 0){
                 mascotas = _context.Mascotas.Include(x => x.Tipo)
                             .Where(x => x.TipoId == tipomascota).ToList();
+            }
+
+            if(sexomascota != 2){
+                if(sexomascota == 1){
+                    mascotas = _context.Mascotas.Where(x => x.Sexo == sexomascota.ToString()).ToList();
+                }else{
+                    mascotas = _context.Mascotas.Where(x => x.Sexo == sexomascota.ToString()).ToList();
+                }
+            }
+
+            if(edadmascota != 0){
+                for(var i = 1; i < 11; i++){
+                    if(edadmascota == i){
+                        mascotas = _context.Mascotas.Where(x => x.Edad == edadmascota).ToList();
+                    }
+                }
             }
 
             return View(mascotas);
